518620567d033255421ef321ea53da0b6ece688e
Alexey Stepanov <Raddosx@gmail.com>
2020-08-10 21:41:09 +0300

    Update single linked list tests

--- test/single-linked-list.test.ts
+++ test/single-linked-list.test.ts
@@ -1,79 +1,111 @@
-import SingleLinkedList from "../src/data-structures/SingleLinkedList";
+import SingleLinkedList from "../src/data-structures/SingleLinkedList/SingleLinkedList";
+import ILinkedList from "../src/data-structures/ILinkedList";
+import ISingleLinkedList from "../src/data-structures/SingleLinkedList/ISingleLinkedList";
 
-test('Single linked list: should correct add elements', () => {
-  const testArr = [5,10,15,20,25];
 
-  const list = new SingleLinkedList<number>();
-  list.createFromArray(testArr);
-  const listData = list.getAsArray();
+describe('Single linked list: addition', () => {
+  const list: ILinkedList<number> = new SingleLinkedList<number>();
 
-  expect(listData).toEqual(testArr);
+  it('should add elements from array', () => {
+    const arr = [1, 2];
+    list.pushFromArray(arr);
+    expect(list.getAsArray()).toEqual(arr);
+  });
+
+  it('should push to list end', () => {
+    list.push(3);
+    expect(list.getAsArray()).toEqual([1, 2, 3]);
+  });
+
+  it('should push to list start', () => {
+    list.unshift(0);
+    expect(list.getAsArray()).toEqual([0, 1, 2, 3]);
+  });
+
 });
 
-test('Single linked list: should correct delete elements', () => {
-  const testArr = [5,10,15,20,25,15];
-  const testArrAfterDeletions = [5,10,20,25];
 
-  const list = new SingleLinkedList<number>();
-  list.createFromArray(testArr);
-  list.deleteNodesByValue(15);
-  const listData = list.getAsArray();
+describe('Single linked list: accession', () => {
+  const emptyList: ISingleLinkedList<number> = new SingleLinkedList<number>();
+  const list: ILinkedList<number> = new SingleLinkedList<number>();
+  list.pushFromArray([10, 20, 30, 40, 50]);
 
-  expect(listData).toEqual(testArrAfterDeletions);
+  it('should get first element', () => {
+    expect(list.peekTail()).toBe(10);
+  });
+
+  it('should get last element', () => {
+    expect(list.peekHead()).toBe(50);
+  });
+
+  it('should get element by index', () => {
+    expect(list.getByIndex(0)).toBe(10);
+    expect(list.getByIndex(2)).toBe(30);
+    expect(() => {
+      list.getByIndex(1000)
+    }).toThrow('Index exceed list length');
+  });
+
+  it('should get first element (empty)', () => {
+    expect(() => {
+      emptyList.peekTail();
+    }).toThrow('Tail does not exist');
+    expect(emptyList.tail).toBe(null);
+  });
+
+  it('should get last element (empty)', () => {
+    expect(() => {
+      emptyList.peekHead();
+    }).toThrow('Head does not exist');
+    expect(emptyList.head).toBe(null);
+  });
+
+  it('should get element by index (empty)', () => {
+    expect(() => {
+      emptyList.getByIndex(0)
+    }).toThrow('List is empty');
+  });
+
 });
 
-test('Single linked list: should unshift correctly', () => {
-  const testArr = [1,2,3,4,5];
-  const testArrAfterUnshift = [0,1,2,3,4,5];
 
-  const list = new SingleLinkedList<number>();
+describe('Single linked list: deletions', () => {
+  const list: ILinkedList<number> = new SingleLinkedList<number>();
+  list.pushFromArray([10, 40, 20, 30, 40, 50, 20, 30]);
 
-  list.createFromArray(testArr);
-  list.unshift(0);
+  it('should delete first element', () => {
+    const shifted = list.shift();
 
-  const listData = list.getAsArray();
+    expect(shifted).toBe(10);
+    expect(list.getAsArray()).toEqual([40, 20, 30, 40, 50, 20, 30]);
+  });
 
-  expect(listData).toEqual(testArrAfterUnshift);
+  it('should delete last element', () => {
+    const popped = list.pop();
+
+    expect(popped).toBe(30);
+    expect(list.getAsArray()).toEqual([40, 20, 30, 40, 50, 20]);
+  });
 });
 
-test('Single linked list: should shift correctly', () => {
-  const testArr = [1,2,3,4,5];
-  const testArrAfterShift = [2,3,4,5];
 
-  const list = new SingleLinkedList<number>();
-  list.createFromArray(testArr);
+describe('Single linked list: iterator', () => {
+  const list: ISingleLinkedList<number> = new SingleLinkedList<number>();
+  const testArray: Array<number> = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 
-  const shifted = list.shift();
-  const listData = list.getAsArray();
+  list.pushFromArray(testArray);
 
-  expect(shifted).toBe(1);
-  expect(listData).toEqual(testArrAfterShift);
+  const iterator1 = list.iterator(0);
+  const iterator2 = list.iterator(5);
+
+  it('should return element data at current position', () => {
+    expect(iterator1.current()).toBe(10);
+    expect(iterator2.current()).toBe(60);
+  });
+
+  it('should iterate to next', () => {
+    expect(iterator1.next()).toBe(20);
+    expect(iterator2.next()).toBe(70);
+  });
+
 });
-
-test('Single linked list: should pop correctly', () => {
-  const testArr = [0,1,2,3,4,5];
-  const testArrAfterShift = [0,1,2,3,4];
-
-  const list = new SingleLinkedList<number>();
-  list.createFromArray(testArr);
-
-  const popped = list.pop();
-  const listData = list.getAsArray();
-
-  expect(popped).toBe(5);
-  expect(listData).toEqual(testArrAfterShift);
-});
-
-test('Single linked list: should peek head and tail correctly', () => {
-  const testArr = [0,1,2,3,4,5];
-
-  const list = new SingleLinkedList<number>();
-  list.createFromArray(testArr);
-
-  const head = list.peekHead();
-  const tail = list.peekTail();
-
-  expect(head).toBe(5);
-  expect(tail).toBe(0);
-});
-
