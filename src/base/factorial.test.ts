1a58f29fc9d7f3f39200b7a168d4815dd4488cab
Alexey Stepanov <Raddosx@gmail.com>
2020-09-13 19:41:42 +0300

    Add factorial memoization

--- src/base/factorial.test.ts
+++ src/base/factorial.test.ts
@@ -1,8 +1,33 @@
-import factorial from "./factorial";
+import {factorial, createMemoizedFactorial} from "./factorial";
 
-test('Factorial: should calc factorial correctly', () => {
-  expect(factorial(5)).toBe(120);
-  expect(factorial(6)).toBe(720);
-  expect(factorial(10)).toBe(3628800);
-  expect(factorial(15)).toBe(1307674368000);
-})
+describe('Factorial', () => {
+
+  const results = {
+    5: 120,
+    6: 720,
+    10: 3628800,
+    15:1307674368000,
+    20:2432902008176640000,
+    25:15511210043330985984000000,
+  };
+
+  test('when is not memoized', () => {
+    expect(factorial(5)).toBe(results[5]);
+    expect(factorial(6)).toBe(results[6]);
+    expect(factorial(10)).toBe(results[10]);
+    expect(factorial(15)).toBe(results[15]);
+    expect(factorial(20)).toBe(results[20]);
+    expect(factorial(25)).toBe(results[25]);
+  });
+
+  test('when is memoized', () => {
+    const memoizedFactorial = createMemoizedFactorial();
+
+    expect(memoizedFactorial(5)).toBe(results[5]);
+    expect(memoizedFactorial(6)).toBe(results[6]);
+    expect(memoizedFactorial(10)).toBe(results[10]);
+    expect(memoizedFactorial(15)).toBe(results[15]);
+    expect(memoizedFactorial(20)).toBe(results[20]);
+    expect(memoizedFactorial(25)).toBe(results[25]);
+  });
+});
--- src/base/factorial.ts
+++ src/base/factorial.ts
@@ -1,3 +1,15 @@
-export default function factorial(x: number): number {
+export function factorial(x: number): number {
   return x > 1 ? x * factorial(x - 1) : x;
+}
+
+export function createMemoizedFactorial(): Function {
+  const cache: Map<number, number> = new Map<number, number>();
+
+  return function (x: number): number {
+    if (!cache.has(x)) {
+      cache.set(x, factorial(x));
+    }
+
+    return <number>cache.get(x);
+  }
 }
