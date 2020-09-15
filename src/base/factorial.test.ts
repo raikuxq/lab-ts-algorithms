adc5cb4db1c14f47eba7690ab2fa955c229e91a7
Alexey Stepanov <Raddosx@gmail.com>
2020-09-15 10:50:53 +0300

    Fix factorial memoization

--- src/base/factorial.test.ts
+++ src/base/factorial.test.ts
@@ -1,33 +1,32 @@
 import {factorial, createMemoizedFactorial} from "./factorial";
 
 describe('Factorial', () => {
+  const results = new Map<number, number>();
+  results
+    .set(5, 120)
+    .set(6, 720)
+    .set(10, 3628800)
+    .set(15, 1307674368000)
+    .set(20, 2432902008176640000)
+    .set(25, 15511210043330985984000000)
 
-  const results = {
-    5: 120,
-    6: 720,
-    10: 3628800,
-    15:1307674368000,
-    20:2432902008176640000,
-    25:15511210043330985984000000,
-  };
+  const resultsKeys = Array.from(results.keys());
 
-  test('when is not memoized', () => {
-    expect(factorial(5)).toBe(results[5]);
-    expect(factorial(6)).toBe(results[6]);
-    expect(factorial(10)).toBe(results[10]);
-    expect(factorial(15)).toBe(results[15]);
-    expect(factorial(20)).toBe(results[20]);
-    expect(factorial(25)).toBe(results[25]);
+
+  describe('with memoize', () => {
+    test.each(resultsKeys)('with n = %i', (n) => {
+      expect(factorial(n)).toBe(results.get(n));
+    });
   });
 
-  test('when is memoized', () => {
-    const memoizedFactorial = createMemoizedFactorial();
 
-    expect(memoizedFactorial(5)).toBe(results[5]);
-    expect(memoizedFactorial(6)).toBe(results[6]);
-    expect(memoizedFactorial(10)).toBe(results[10]);
-    expect(memoizedFactorial(15)).toBe(results[15]);
-    expect(memoizedFactorial(20)).toBe(results[20]);
-    expect(memoizedFactorial(25)).toBe(results[25]);
+  describe('with memoize', () => {
+    test.each(resultsKeys)('with n = %i', (n) => {
+      const memoizedFactorial = createMemoizedFactorial();
+
+      expect(memoizedFactorial(n)).toBe(results.get(n));
+    });
   });
+
+
 });
