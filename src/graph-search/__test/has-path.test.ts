0d8c9dcdcd5f4faff38ca44a0c97fe53d56f4d0c
Alexey Stepanov <Raddosx@gmail.com>
2020-10-11 21:19:01 +0300

    Refactor has-path algorithm

--- src/graph-search/__test/has-path.test.ts
+++ src/graph-search/__test/has-path.test.ts
@@ -6,73 +6,111 @@ import BFSIterationStrategy from "../../data-structures/Graph/strategy/BFSIterat
 import DFSIterationStrategy from "../../data-structures/Graph/strategy/DFSIterationStrategy";
 
 import hasPath from "../has-path";
+import shortestPath from "../shortest-path";
 
-describe.each(["BFS", "DFS"])(
-  "has path searching by %s method",
-  (strategyType: string) => {
-    let strategy: IGraphIterationStrategy<string>;
+describe("has path searching algorithm", () => {
+  describe("in any graph type", () => {
+    const strategy: IGraphIterationStrategy<string> = new BFSIterationStrategy();
 
-    switch (strategyType) {
-      case "BFS":
-        strategy = new BFSIterationStrategy();
-        break;
-      case "DFS":
-        strategy = new DFSIterationStrategy();
-        break;
-      default:
-        throw new Error("Invalid search method");
+    describe("in empty graph", () => {
+      const graph: IGraph<string> = new Graph();
+
+      test("should throw when graph is empty", () => {
+        expect(() => {
+          shortestPath(graph, "-", "-", strategy);
+        }).toThrowError();
+      });
+    });
+
+    describe("in non empty graph", () => {
+      const graph: IGraph<string> = new Graph();
+
+      graph.addVertex("Mike").addVertex("Bob").addEdge("Mike", "Bob");
+
+      test("should throw when first node does not exist", () => {
+        expect(() => {
+          shortestPath(graph, "Mike", "NOT_EXISTED_NODE", strategy);
+        }).toThrowError();
+      });
+
+      test("should throw when second node does not exist", () => {
+        expect(() => {
+          shortestPath(graph, "NOT_EXISTED_NODE", "Bob", strategy);
+        }).toThrowError();
+      });
+    });
+  });
+
+  describe.each(["BFS", "DFS"])(
+    "searching by %s method",
+    (strategyType: string) => {
+      let strategy: IGraphIterationStrategy<string>;
+
+      switch (strategyType) {
+        case "BFS":
+          strategy = new BFSIterationStrategy();
+          break;
+        case "DFS":
+          strategy = new DFSIterationStrategy();
+          break;
+        default:
+          throw new Error("Invalid search method");
+      }
+
+      describe("in undirected graph", () => {
+        const graph: IGraph<string> = new Graph();
+
+        graph
+          .addVertex("Mike")
+          .addVertex("Bob")
+          .addVertex("Lisa")
+          .addVertex("Aaron")
+          .addVertex("James")
+          .addVertex("Anna")
+          .addVertex("John")
+          .addEdge("Mike", "Bob")
+          .addEdge("Mike", "Lisa")
+          .addEdge("Lisa", "James")
+          .addEdge("Lisa", "Aaron")
+          .addEdge("James", "Aaron")
+          .addEdge("James", "Anna");
+
+        test("should find element if it has an edge", () => {
+          expect(hasPath(graph, "Mike", "Anna", strategy)).toBe(true);
+        });
+        test("should not find element if it has not an edge", () => {
+          expect(hasPath(graph, "Mike", "John", strategy)).toBe(false);
+        });
+      });
+
+      describe("in directed graph", () => {
+        const graph: IGraph<string> = new Graph(true);
+
+        graph
+          .addVertex("Mike")
+          .addVertex("Bob")
+          .addVertex("Lisa")
+          .addVertex("Aaron")
+          .addVertex("James")
+          .addVertex("Anna")
+          .addVertex("John")
+          .addEdge("Mike", "Bob")
+          .addEdge("Mike", "Lisa")
+          .addEdge("Lisa", "James")
+          .addEdge("Lisa", "Aaron")
+          .addEdge("James", "Aaron")
+          .addEdge("James", "Anna");
+
+        test("should find element if it has an edge", () => {
+          expect(hasPath(graph, "Mike", "Anna", strategy)).toBe(true);
+        });
+        test("should not find element if it has not an edge", () => {
+          expect(hasPath(graph, "Mike", "John", strategy)).toBe(false);
+        });
+        test("should not find element if and edge has wrong direction", () => {
+          expect(hasPath(graph, "Lisa", "Mike", strategy)).toBe(false);
+        });
+      });
     }
-
-    test("when graph is empty", () => {
-      const graph: IGraph<string> = new Graph<string>();
-
-      expect(() => {
-        hasPath(graph, "Asd", "Qwe", strategy);
-      }).toThrow("Start vertex does not exist");
-    });
-
-    test("when graph is undirected", () => {
-      const graph: IGraph<string> = new Graph<string>();
-
-      graph
-        .addVertex("Mike")
-        .addVertex("Bob")
-        .addVertex("Lisa")
-        .addVertex("Aaron")
-        .addVertex("James")
-        .addVertex("Anna")
-        .addEdge("Mike", "Bob")
-        .addEdge("Mike", "Lisa")
-        .addEdge("Lisa", "James")
-        .addEdge("Lisa", "Aaron")
-        .addEdge("James", "Aaron")
-        .addEdge("James", "Anna");
-
-      expect(hasPath(graph, "Mike", "Anna", strategy)).toBeTruthy();
-      expect(hasPath(graph, "Mike", "John", strategy)).toBeFalsy();
-    });
-
-    test("when graph is directed", () => {
-      const graph: IGraph<string> = new Graph<string>(true);
-
-      graph
-        .addVertex("Mike")
-        .addVertex("Bob")
-        .addVertex("Lisa")
-        .addVertex("Aaron")
-        .addVertex("James")
-        .addVertex("Anna")
-        .addEdge("Mike", "Bob")
-        .addEdge("Mike", "Lisa")
-        .addEdge("Lisa", "Aaron")
-        .addEdge("James", "Aaron")
-        .addEdge("James", "Anna")
-        .addEdge("Aaron", "Anna");
-
-      expect(hasPath(graph, "Mike", "Anna", strategy)).toBeTruthy();
-      expect(hasPath(graph, "Mike", "Aaron", strategy)).toBeTruthy();
-      expect(hasPath(graph, "Lisa", "Bob", strategy)).toBeFalsy();
-      expect(hasPath(graph, "Lisa", "James", strategy)).toBeFalsy();
-    });
-  }
-);
+  );
+});
--- src/graph-search/has-path.ts
+++ src/graph-search/has-path.ts
@@ -1,6 +1,6 @@
 import IGraph from "../data-structures/Graph/interface/IGraph";
-import IGraphIterationStrategy from "../data-structures/Graph/strategy/interface/IGraphIterationStrategy";
 import IGraphIterator from "../data-structures/Graph/iterator/interface/IGraphIterator";
+import IGraphIterationStrategy from "../data-structures/Graph/strategy/interface/IGraphIterationStrategy";
 
 export default function hasPath<V>(
   graph: IGraph<V>,
@@ -10,6 +10,16 @@ export default function hasPath<V>(
 ): boolean {
   const iterator: IGraphIterator<V> = strategy.createIterator(graph, from);
 
+  /**
+   * Validate
+   */
+  if (!graph.hasVertex(from) || !graph.hasVertex(to)) {
+    throw new Error("Invalid arguments: no such elements");
+  }
+
+  /**
+   * Find target element
+   */
   while (iterator.hasNext()) {
     const next = iterator.next();
 
