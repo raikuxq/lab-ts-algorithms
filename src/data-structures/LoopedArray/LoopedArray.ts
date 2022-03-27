e3fae73b5c06213023a7891cb553f6c127f03ce8
Raiku <raikulmao.dev@gmail.com>
2022-03-28 00:25:01 +0300

    Update looped array to be compatible with new interface

--- src/data-structures/LoopedArray/LoopedArray.ts
+++ src/data-structures/LoopedArray/LoopedArray.ts
@@ -1,37 +1,60 @@
-import ICollection from "../../types/ICollection";
+import ILinearStorageAccessible from "../../types/ILinearStorageAccessible";
 
-export default class LoopedArray<T> implements ICollection<T> {
-  private _realLength = 0;
+/**
+ * Linear data structure
+ * Facade above array
+ * After reaching full array new pushed elements will be overwritten over old elements
+ */
+export default class LoopedArray<T> implements ILinearStorageAccessible<T> {
   private readonly _capacity: number;
+  private _realLength = 0;
   private _array: Array<T>;
 
+  /**
+   * Create empty instance
+   */
   constructor(capacity: number) {
+    if (capacity <= 0) {
+      throw new Error("Capacity must be larger than 0");
+    }
     this._capacity = capacity;
     this._array = new Array<T>(0);
   }
 
-  public push(item: T): void {
+  /**
+   * Push into end
+   * @param value - data
+   */
+  public push(value: T): void {
     this._realLength++;
 
-    if (this._array.length < this._capacity) {
-      this._array.push(item);
+    if (!this.isFull()) {
+      this._array.push(value);
     } else {
       const indexToPush = (this._realLength % this._capacity) - 1;
-      this._array.splice(indexToPush, 1, item);
+      this._array.splice(indexToPush, 1, value);
     }
   }
 
-  public unshift(item: T): void {
+  /**
+   * Push into start
+   * @param value - data
+   */
+  public unshift(value: T): void {
     this._realLength++;
 
-    if (this._array.length < this._capacity) {
-      this._array.unshift(item);
+    if (!this.isFull()) {
+      this._array.unshift(value);
     } else {
       this._array.splice(this._capacity - 1);
-      this._array.unshift(item);
+      this._array.unshift(value);
     }
   }
 
+  /**
+   * Delete node from list's end
+   * @returns data of deleted element
+   */
   public pop(): T {
     this._realLength--;
     const deletedItem = this._array.pop();
@@ -42,6 +65,10 @@ export default class LoopedArray<T> implements ICollection<T> {
     return deletedItem;
   }
 
+  /**
+   * Delete node from list's start
+   * @returns data of deleted element
+   */
   public shift(): T {
     this._realLength--;
     const deletedItem = this._array.shift();
@@ -52,27 +79,114 @@ export default class LoopedArray<T> implements ICollection<T> {
     return deletedItem;
   }
 
-  public getArray(): Array<T> {
-    return this._array;
-  }
-
-  public peekHead(): T {
+  /**
+   * Get head element data
+   * @throws Error when head does not exists
+   * @returns data of picked element
+   */
+  public peek(): T {
     return this._array[this._array.length - 1];
   }
 
-  public peekTail(): T {
+  /**
+   * Get tail element data
+   * @throws Error when head does not exists
+   * @returns data of picked element
+   */
+  public peekFromStart(): T {
     return this._array[0];
   }
 
+  /**
+   * Get list element by index from start
+   * @throws when element does not exists
+   * @returns data of picked element
+   */
+  peekByIndex(index: number): T {
+    return this._array[index];
+  }
+
+  /**
+   * Push from index
+   * @param value - data
+   * @param fromIndex - index from start
+   */
+  pushFromIndex(value: T, fromIndex: number): void {
+    this._array[fromIndex] = value;
+  }
+
+  /**
+   * Get elements as array
+   * @returns array representation of list
+   */
+  public getAsArray(): Array<T> {
+    return this._array;
+  }
+
+  /**
+   * Check if element exists in list
+   * @param item
+   * @returns boolean
+   */
+  public has(item: T): boolean {
+    return this._array.includes(item);
+  }
+
+  /**
+   * Is list empty
+   * @returns boolean
+   */
   public isEmpty(): boolean {
     return this._array.length === 0;
   }
 
+  /**
+   * Is list full
+   * @returns boolean
+   */
+  public isFull(): boolean {
+    return this._array.length >= this._capacity;
+  }
+
+  /**
+   * List length
+   * @returns number - quantity of list's elements
+   */
   public length(): number {
     return this._array.length;
   }
 
+  /**
+   * Remove all elements from list
+   */
   public clear(): void {
     this._array = new Array<T>(0);
+  }
+
+  /**
+   * Delete node from list by index from start
+   * @returns data of deleted element
+   */
+  deleteFromIndex(fromIndex: number): T {
+    const deletedElement = this._array[fromIndex];
+    delete this._array[fromIndex];
+    return deletedElement;
+  }
+
+  /**
+   * Add elements to list from array
+   * @param elements - array of elements to push
+   * */
+  pushFromArray(elements: Array<T>): void {
+    elements.forEach((element: T) => {
+      this.push(element);
+    });
+  }
+
+  /**
+   * Reverse list nodes links and swap head with tail
+   */
+  reverse(): void {
+    this._array = this._array.reverse();
   }
 }
--- test/looped-array.test.ts
+++ test/looped-array.test.ts
@@ -1,6 +1,17 @@
 import LoopedArray from "../src/data-structures/LoopedArray/LoopedArray";
+import ILinearStorageAccessible from "../src/types/ILinearStorageAccessible";
 
 describe("Looped Array", () => {
+  describe("constructor", () => {
+    test("should throw when capacity is less than 1", () => {
+      expect(() => {
+        new LoopedArray(-5);
+      }).toThrowError();
+      expect(() => {
+        new LoopedArray(0);
+      }).toThrowError();
+    });
+  });
   describe("method push", () => {
     const loopedArray = new LoopedArray(5);
 
@@ -10,8 +21,28 @@ describe("Looped Array", () => {
     loopedArray.push("r");
     loopedArray.push("t");
 
+    test("should add elements to array's end", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+
+      array.push(1);
+      expect(array.peek()).toBe(1);
+    });
+    test("should overwrite when array is full", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        2
+      );
+
+      array.push(1);
+      array.push(2);
+      array.push(3);
+
+      expect(array.getAsArray()).toEqual([3, 2]);
+    });
+
     test("should collect correct elements before overwriting", () => {
-      expect(loopedArray.getArray()).toEqual(["q", "w", "e", "r", "t"]);
+      expect(loopedArray.getAsArray()).toEqual(["q", "w", "e", "r", "t"]);
     });
 
     test("should correctly overwrite ", () => {
@@ -19,7 +50,7 @@ describe("Looped Array", () => {
       loopedArray.push("u");
       loopedArray.push("i");
 
-      expect(loopedArray.getArray()).toEqual(["y", "u", "i", "r", "t"]);
+      expect(loopedArray.getAsArray()).toEqual(["y", "u", "i", "r", "t"]);
     });
   });
 
@@ -33,7 +64,7 @@ describe("Looped Array", () => {
     loopedArray.unshift("t");
 
     test("should collect correct elements before overwriting", () => {
-      expect(loopedArray.getArray()).toEqual(["t", "r", "e", "w", "q"]);
+      expect(loopedArray.getAsArray()).toEqual(["t", "r", "e", "w", "q"]);
     });
 
     test("should correctly overwrite ", () => {
@@ -41,28 +72,89 @@ describe("Looped Array", () => {
       loopedArray.unshift("u");
       loopedArray.unshift("i");
 
-      expect(loopedArray.getArray()).toEqual(["i", "u", "y", "t", "r"]);
+      expect(loopedArray.getAsArray()).toEqual(["i", "u", "y", "t", "r"]);
+    });
+
+    test("should add elements to array's start", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+
+      array.unshift(1);
+      array.unshift(0);
+      expect(array.peekFromStart()).toBe(0);
+    });
+    test("should overwrite when array is full", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        2
+      );
+      array.unshift(1);
+      array.unshift(2);
+      array.unshift(3);
+      expect(array.getAsArray()).toEqual([3, 2]);
     });
   });
 
-  describe("method peekHead", () => {
-    test("should return last element of array", () => {
-      const loopedArray = new LoopedArray(5);
+  describe("method pushFromIndex", () => {
+    test("should overwrite elements by index", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
 
-      loopedArray.push("q");
-      loopedArray.push("w");
-      loopedArray.push("e");
-
-      expect(loopedArray.peekHead()).toEqual("e");
+      array.pushFromArray([10, 30, 40, 50]);
+      array.pushFromIndex(20, 1);
+      expect(array.getAsArray()).toEqual([10, 20, 40, 50]);
     });
-    test("should return undefined when array is empty", () => {
-      const loopedArray = new LoopedArray(5);
+    test("should add elements to array from start", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
 
-      expect(loopedArray.peekHead()).toEqual(undefined);
+      const expectedArr: Array<number> = [0, 10];
+      array.pushFromArray([0]);
+      array.pushFromIndex(10, 1);
+      expect(array.getAsArray()).toEqual(expectedArr);
+    });
+    test("should add elements to empty array", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+
+      const expectedArr: Array<number> = [10];
+      array.pushFromIndex(10, 0);
+      expect(array.getAsArray()).toEqual(expectedArr);
+    });
+    test("should add elements to array from end", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+
+      array.pushFromArray([0, 10, 30]);
+      array.pushFromIndex(20, 2);
+      expect(array.getAsArray()).toEqual([0, 10, 20]);
     });
   });
 
-  describe("method peekTail", () => {
+  describe("method pushFromArray", () => {
+    test("should add elements to array's end", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      const arr = [1, 2, 3, 4, 5];
+      array.pushFromArray(arr);
+      expect(array.getAsArray()).toEqual(arr);
+    });
+    test("should overwrite when array is full", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        2
+      );
+      array.pushFromArray([1]);
+      array.pushFromArray([2, 3]);
+      expect(array.getAsArray()).toEqual([3, 2]);
+    });
+  });
+
+  describe("method peekFromStart", () => {
     test("should return first element of array", () => {
       const loopedArray = new LoopedArray(5);
 
@@ -70,12 +162,189 @@ describe("Looped Array", () => {
       loopedArray.push("w");
       loopedArray.push("e");
 
-      expect(loopedArray.peekTail()).toEqual("q");
+      expect(loopedArray.peekFromStart()).toEqual("q");
     });
     test("should return undefined when array is empty", () => {
       const loopedArray = new LoopedArray(5);
 
-      expect(loopedArray.peekTail()).toEqual(undefined);
+      expect(loopedArray.peekFromStart()).toEqual(undefined);
+    });
+    test("should return undefined when array is empty", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      expect(array.peekFromStart()).toBeUndefined();
+    });
+    test("should return first element from array", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray(5);
+      array.pushFromArray([10, 20, 30, 40, 50]);
+
+      expect(array.peekFromStart()).toBe(10);
+    });
+  });
+
+  describe("method peek", () => {
+    test("should return last element of array", () => {
+      const loopedArray = new LoopedArray(5);
+
+      loopedArray.push("q");
+      loopedArray.push("w");
+      loopedArray.push("e");
+
+      expect(loopedArray.peek()).toEqual("e");
+    });
+    test("should return undefined when array is empty", () => {
+      const loopedArray = new LoopedArray(5);
+
+      expect(loopedArray.peek()).toEqual(undefined);
+    });
+    test("should return undefined when array is empty", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      expect(array.peek()).toBeUndefined();
+    });
+    test("should return first element from array", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      array.pushFromArray([10, 20, 30, 40, 50]);
+
+      expect(array.peek()).toBe(50);
+    });
+  });
+
+  describe("method peekByIndex", () => {
+    test("should throw when array is empty", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      expect(array.peekByIndex(0)).toBeUndefined();
+    });
+    test("should return element by its index from array", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      array.pushFromArray([10, 20, 30, 40, 50]);
+
+      expect(array.peekByIndex(2)).toBe(30);
+    });
+    test("should return undefined when index exceed array length", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+
+      expect(array.peekByIndex(1000)).toBeUndefined();
+    });
+  });
+
+  describe("method shift", () => {
+    describe("should delete first element and return its value", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      array.pushFromArray([10, 20]);
+      const shifted = array.shift();
+
+      test("should delete correct", () => {
+        expect(array.getAsArray()).toEqual([20]);
+      });
+      test("should return correct value", () => {
+        expect(shifted).toBe(10);
+      });
+    });
+
+    test("should throw when array is empty", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      expect(() => {
+        array.shift();
+      }).toThrowError();
+    });
+  });
+
+  describe("method pop", () => {
+    describe("should delete last element and return its value", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      array.pushFromArray([10, 40]);
+      const shifted = array.pop();
+
+      test("should delete correct", () => {
+        expect(array.getAsArray()).toEqual([10]);
+      });
+
+      test("should return correct value", () => {
+        expect(shifted).toBe(40);
+      });
+    });
+
+    test("should throw when array is empty", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+
+      expect(() => {
+        array.pop();
+      }).toThrowError();
+    });
+  });
+
+  describe("method deleteFromIndex", () => {
+    describe("should replace element with undefined by index and return its value", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+      array.pushFromArray([10, 20, 30]);
+      const shifted = array.deleteFromIndex(1);
+
+      test("should delete correct", () => {
+        expect(array.getAsArray()).toEqual([10, undefined, 30]);
+      });
+      test("should return correct value", () => {
+        expect(shifted).toBe(20);
+      });
+    });
+
+    test("should throw when array is empty", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+
+      expect(() => {
+        array.shift();
+      }).toThrowError();
+    });
+  });
+
+  describe("method reverse", () => {
+    test("should correct reverse array", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        12
+      );
+
+      const arraySource = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110];
+      const reversedArray = [...arraySource].reverse();
+      array.pushFromArray(arraySource);
+      array.reverse();
+
+      expect(array.getAsArray()).toEqual(reversedArray);
+    });
+  });
+
+  describe("method clear", () => {
+    test("should correct clear array", () => {
+      const array: ILinearStorageAccessible<number> = new LoopedArray<number>(
+        10
+      );
+
+      const testArray: Array<number> = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+      array.pushFromArray(testArray);
+      array.clear();
+
+      expect(array.getAsArray()).toHaveLength(0);
     });
   });
 });
