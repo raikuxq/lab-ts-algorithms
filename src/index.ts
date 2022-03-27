eb1d42638b4d8fd8114fee2b4cabbf2b4d90a841
Raiku <raikulmao.dev@gmail.com>
2022-03-28 00:29:22 +0300

    Add demonstrations into entry point file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 /**
  * Write your code here
  */
+
+import { demoLinkedList } from "./demo/demo.linked-list";
+import { demoQueue } from "./demo/demo.queue";
+import { demoLoopedArray } from "./demo/demo.looped-array";
+import { demoStack } from "./demo/demo.stack";
+
+demoLinkedList();
+console.log("================================================================");
+demoLoopedArray();
+console.log("================================================================");
+demoQueue();
+console.log("================================================================");
+demoStack();
+console.log("================================================================");
