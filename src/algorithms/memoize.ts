e36e0b727458de586aa2cc4ddf8985204ad96413
Raiku <raikulmao.dev@gmail.com>
2022-05-12 15:28:14 +0300

    Move memoize util into separated file

---
+++ src/algorithms/memoize.ts
@@ -0,0 +1,21 @@
+import { FnToMemoize } from "../types/FnToMemoize";
+
+/**
+ * Wrapper function that storing the results of calls and returning the cached result when the same inputs occur again
+ */
+export const memoize = <Key, Value>(
+  fn: FnToMemoize<Key, Value>
+): FnToMemoize<Key, Value> => {
+  const cache = new Map<string, Value>();
+
+  return (...args: Array<Key>): Value => {
+    const jsonArgs = JSON.stringify(args);
+
+    if (!cache.has(jsonArgs)) {
+      const result = fn(...args);
+      cache.set(jsonArgs, result);
+    }
+
+    return <Value>cache.get(jsonArgs);
+  };
+};
