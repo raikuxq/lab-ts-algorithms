9ce998ba0ae287f52301e33921fb92f9d6e4630f
Raiku <raikulmao.dev@gmail.com>
2022-05-12 15:34:12 +0300

    Update index.ts - add new demos

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,40 @@
 /**
  * Write your code here
  */
-
-import { perfQueue, perfStack } from "./performance/ds-compare";
-import { demoLinkedList } from "./demo/demo.linked-list";
-import { demoQueue } from "./demo/demo.queue";
-import { demoLoopedArray } from "./demo/demo.looped-array";
-import { demoStack } from "./demo/demo.stack";
-import { compareAllSortTypes } from "./performance/sort-compare";
-import {
-  demoUndirectedGraph,
-  demoGraphGenerated,
-  demoDirectedGraph,
-} from "./demo/demo.graph";
-
+// import { demoBst, demoBstObjects } from "./demo/demo.bst";
+// import { bstCompare } from "./demo/performance/bst-compare";
+// import { demoStack } from "./demo/demo.stack";
+// import { perfQueue, perfStack } from "./demo/performance/ds-compare";
+// import { demoLinkedList } from "./demo/demo.linked-list";
+// import { demoQueue } from "./demo/demo.queue";
+// import {
+//   demoDirectedGraph,
+//   demoGraphGenerated,
+//   demoUndirectedGraph,
+// } from "./demo/demo.graph";
+// import { demoLoopedArray } from "./demo/demo.looped-array";
+// import { compareAllSortTypes } from "./demo/performance/sort-compare";
+//
 // demoLinkedList();
-// console.log("================================================================");
+// // console.log("================================================================");
 // demoLoopedArray();
-// console.log("================================================================");
+// // console.log("================================================================");
 // demoQueue();
-// console.log("================================================================");
+// // console.log("================================================================");
 // demoStack();
-// console.log("================================================================");
+// // console.log("================================================================");
 // demoUndirectedGraph();
-// console.log("================================================================");
+// // console.log("================================================================");
 // demoDirectedGraph();
-// console.log("================================================================");
+// // console.log("================================================================");
 // demoGraphGenerated();
-// console.log("================================================================");
+// // console.log("================================================================");
 // compareAllSortTypes();
-// console.log("================================================================");
+// // console.log("===========================================================");
 // perfQueue();
-// console.log("================================================================");
+// // console.log("===========================================================");
 // perfStack();
-// console.log("================================================================");
+// // console.log("===========================================================");
+// demoBst();
+// // console.log("===========================================================");
+// bstCompare();
