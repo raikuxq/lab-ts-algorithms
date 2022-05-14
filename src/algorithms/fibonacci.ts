6114211a27004904a145476a68c7f70cc7772e74
Raiku <raikulmao.dev@gmail.com>
2022-05-14 17:46:25 +0300

    Add fibonacci jsdoc comment

--- src/algorithms/fibonacci.ts
+++ src/algorithms/fibonacci.ts
@@ -1,10 +1,16 @@
 import { FnToMemoize } from "../types/FnToMemoize";
 import { memoize } from "./memoize";
 
+/**
+ * Time complexity: O(n!)
+ */
 export const fibonacci = (n: number): number => {
   return n > 1 ? fibonacci(n - 1) + fibonacci(n - 2) : n;
 };
 
+/**
+ * Time complexity: O(n) - due to caching
+ */
 export const memoizedFibonacci: FnToMemoize<number, number> = memoize(
   (n: number) => {
     return n > 1 ? memoizedFibonacci(n - 1) + memoizedFibonacci(n - 2) : n;
