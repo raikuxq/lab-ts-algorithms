0536516078f94cc799ef7795bb79c4d0d254ea33
Raiku <raikulmao.dev@gmail.com>
2022-05-14 18:38:45 +0300

    Refactor binary searching test - rename tests

--- test/unit/algorithms/binary-search.test.ts
+++ test/unit/algorithms/binary-search.test.ts
@@ -1,27 +1,23 @@
 import { binarySearch } from "../../../src/algorithms/binary-search";
 
 describe("Binary search", () => {
-  it("find in positive 100 elements", () => {
+  it("should find in positive 100 elements", () => {
     const arr: Array<number> = [];
-
     for (let i = 0; i < 100; i++) arr.push(i + 1);
+    const foundElement = binarySearch(arr, 97);
 
-    const index = binarySearch(arr, 97);
-
-    expect(index).toBe(96);
+    expect(foundElement).toBe(96);
   });
 
-  it("find in negative 100 elements", () => {
+  it("should find in negative 100 elements", () => {
     const arr: Array<number> = [];
-
     for (let i = 0; i < 100; i++) arr.push(-100 + i - 1);
-
     const foundElement = binarySearch(arr, -97);
 
     expect(foundElement).toBe(4);
   });
 
-  it("in empty list", () => {
+  it("should return null in empty list", () => {
     const arr: Array<number> = [];
 
     expect(binarySearch(arr, 10)).toBeNull();
