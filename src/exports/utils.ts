6e6eb3dc865d608b1ea535129207a1a8a18aa18a
Raiku <raikulmao.dev@gmail.com>
2022-05-17 11:54:57 +0300

    Add exports definition

---
+++ src/exports/utils.ts
@@ -0,0 +1,21 @@
+import { memoize } from "../algorithms/memoize";
+import {
+  getMinIndex,
+  getMinIndexFromIndex,
+  perf,
+  randomizeNumberInRange,
+  roundNumber,
+  swapArrayItems,
+  perfAsync,
+} from "../utils";
+
+export {
+  perf,
+  getMinIndex,
+  getMinIndexFromIndex,
+  memoize,
+  perfAsync,
+  roundNumber,
+  randomizeNumberInRange,
+  swapArrayItems,
+};
